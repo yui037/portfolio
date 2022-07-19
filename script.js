5c9cdce0be5c2b88744fb327c0d05e8b708a97ee
yui037 <108321803+yui037@users.noreply.github.com>
2022-07-19 13:54:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Fade Sample ////////////
+$( function() {
+
+  $(".fade").fadeTo(0,0.2); // 初期状態 20%
+
+  $(".fade").hover(
+    function(){
+      $(this).fadeTo("slow", 1.0); // マウスが載ったら100%までフェードする
+    },
+    function(){
+      $(this).fadeTo("slow", 0.2); // マウスが離れたら20%に戻す
+    }
+  );
+
+});
+
+// Animate Sample //////////
+$( function(){
+  $("#left").click( function(){
+    $('#move').animate( { 'left': '-=200px' } , { 'duration': 1000 } );
+  });
+  $("#right").click( function(){
+    $('#move').animate( { 'left': '+=200px' } , { 'duration': 1000 } );
+  });
+});
